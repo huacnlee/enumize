a626e6c2ee6ba5fa24fc6d472844f5ca78de567a
Jason Lee <huacnlee@gmail.com>
2019-06-24 15:10:37 +0800

    Fix tests

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -9,14 +9,11 @@ require "enumize"
 
 module Dummy
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.available_locales = ["en", "zh-CN"]
-    config.i18n.fallbacks = true
+    config.i18n.fallbacks = ["en", "zh-CN"]
   end
 end
